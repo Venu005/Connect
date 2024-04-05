4e84f702d4525a3bf2f5e9bc209698f0af92272a
Venusai <118623801+Venu005@users.noreply.github.com>
2024-04-05 14:38:56 +0530

    fix redirect issue

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,29 @@
-import { SignIn } from "@clerk/nextjs";
-import React from "react";
+"use client";
+import Loader from "@/components/Loader";
+import { SignIn, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const SignInPage = () => {
-  return (
-    <main className="flex h-screen w-full items-center justify-center">
-      <SignIn redirectUrl="/dashboard" />
-    </main>
-  );
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/dashboard");
+    }
+  }, [user, router]);
+
+  if (user) {
+    return <Loader />;
+  } else {
+    return (
+      <main className="flex h-screen w-full items-center justify-center">
+        <SignIn redirectUrl="/dashboard" />
+      </main>
+    );
+  }
 };
 
 export default SignInPage;
+
